238e65ff990a41ea9a7376375e56a07df55b2ef9
DIALLO Thierno Abdoulaye <tadricardo92@gmail.com>
2021-12-21 20:59:17 +0100

    Ajout des controllers et views de Question ,Answer et Theme

---
+++ Quiz.WebUI/Controllers/QuestionsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Quiz.Core.Models;
+using QUIZ.DataAccess.SQL;
+
+namespace Quiz.WebUI.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private MyContext db = new MyContext();
+
+        // GET: Questions
+        public ActionResult Index()
+        {
+            var questions = db.Questions.Include(q => q.Quiz);
+            return View(questions.ToList());
+        }
+
+        // GET: Questions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // GET: Questions/Create
+        public ActionResult Create()
+        {
+            ViewBag.QuizId = new SelectList(db.Quizzes, "Id", "Title");
+            return View();
+        }
+
+        // POST: Questions/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,QstText,IsMultiple,NumOrder,QuizId")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Questions.Add(question);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.QuizId = new SelectList(db.Quizzes, "Id", "Title", question.QuizId);
+            return View(question);
+        }
+
+        // GET: Questions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.QuizId = new SelectList(db.Quizzes, "Id", "Title", question.QuizId);
+            return View(question);
+        }
+
+        // POST: Questions/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,QstText,IsMultiple,NumOrder,QuizId")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(question).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.QuizId = new SelectList(db.Quizzes, "Id", "Title", question.QuizId);
+            return View(question);
+        }
+
+        // GET: Questions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Question question = db.Questions.Find(id);
+            db.Questions.Remove(question);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Quiz.WebUI/Controllers/ThemesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Quiz.Core.Models;
+using QUIZ.DataAccess.SQL;
+
+namespace Quiz.WebUI.Controllers
+{
+    public class ThemesController : Controller
+    {
+        private MyContext db = new MyContext();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.Themes.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Theme theme = db.Themes.Find(id);
+            if (theme == null)
+            {
+                return HttpNotFound();
+            }
+            return View(theme);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ThemeName")] Theme theme)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Themes.Add(theme);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(theme);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Theme theme = db.Themes.Find(id);
+            if (theme== null)
+            {
+                return HttpNotFound();
+            }
+            return View(theme);
+        }
+
+        // POST: Categories/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ThemeName")] Theme theme)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(theme).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(theme);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Theme theme = db.Themes.Find(id);
+            if (theme == null)
+            {
+                return HttpNotFound();
+            }
+            return View(theme);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Theme theme = db.Themes.Find(id);
+            db.Themes.Remove(theme);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
